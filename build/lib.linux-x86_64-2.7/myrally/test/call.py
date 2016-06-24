90152c0a7e13720ee0dda692d72c91df790e3ce5
Harsh <hmdesai89@gmail.com>
2016-06-24 08:34:18 +0000

    test

--- build/lib.linux-x86_64-2.7/myrally/test/call.py
+++
@@ -1,9 +0,0 @@
-from myrally.test_client import client
-from myrally.ipc import queue
-import pdb
-
-pdb.set_trace()
-jclient = client.Client()
-#queue.create_queue()
-print jclient.vpc.describe_vpcs()
-#queue.delete_queue()
--- build/lib.linux-x86_64-2.7/test/read.py
+++
@@ -1,9 +0,0 @@
-from myrally.ipc import queue
-
-
-mq = queue.get_message_queue()
-
-s, _ = mq.receive()
-semaphore = posix_ipc.Semaphore(s)
-semaphore.release()
-
